47916ca31a86f9cde4c34b22aae3721a71790fd1
Kyle Mays <kyleloganmays@gmail.com>
2019-07-11 06:55:01 +0000

    Add an Image

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,14 +3,14 @@ before_action :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merge(user: current_user))
+    @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
 
    private
 
   def photo_params
-    params.require(:picture).permit(:caption)
+    params.require(:photo).permit(:caption, :picture)
   end
 
 
