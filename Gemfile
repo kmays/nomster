89387221919322368948721ae021cfb5777fb3b2
Kyle Mays <kyleloganmays@gmail.com>
2019-07-11 07:16:30 +0000

    Add an Image

--- Gemfile
+++ Gemfile
@@ -80,4 +80,5 @@ gem 'devise'
 gem "geocoder"
 gem "figaro"
 gem 'carrierwave', '~> 1.0'
+gem 'fog-aws'
 
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params)
+    @place.photos.create(photo_params.merge(picture: current_user))
     redirect_to place_path(@place)
   end
 
